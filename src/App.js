c8f54096de1782402296244935ef20819f47ad97
Arif Hussain <arif3337179@gmail.com>
2023-12-19 12:13:57 +0500

    1st

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <>
+    <h1>
+      Hi
+    </h1>
+    </>
+  );
+}
+export default App; 
